dfc3d7c901bda732249765862c3611d700af5af7
ThathsaraD <110688948+Merc-47@users.noreply.github.com>
2026-07-22 10:55:52 +0530

    Organize application layer by feature

---
+++ src/DeployNexus.Application/Common/Result.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeployNexus.Application.Common;
+
+public class Result
+{
+    public bool Success { get; init; }
+
+    public string Message { get; init; } = string.Empty;
+
+    public static Result Ok(string message = "")
+    {
+        return new Result
+        {
+            Success = true,
+            Message = message
+        };
+    }
+
+    public static Result Failure(string message)
+    {
+        return new Result
+        {
+            Success = false,
+            Message = message
+        };
+    }
+}
---
+++ src/DeployNexus.Application/Users/DTOs/CreateUserRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeployNexus.Application.DTOs;
+
+public class CreateUserRequest
+{
+    public string Username { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+}
